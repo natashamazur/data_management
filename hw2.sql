b939371b9328995fbc603842be20c794e541a32f
natashamazur <72361552+natashamazur@users.noreply.github.com>
2020-10-19 00:09:28 +0300

    fix limits in hw2

--- hw2.sql
+++ hw2.sql
@@ -7,10 +7,10 @@ set search_path to movie;
 select * from movie.ratings limit 10;
 
 --выбрать из таблицы links всё записи, у которых imdbid оканчивается на "42", а поле movieid между 100 и 1000
-select * from links where imdbid like '%42' and movieid between 100 and 1000;
+select * from links where imdbid like '%42' and movieid between 100 and 1000 limit 10;
 
 --выбрать из таблицы links все imdbId, которым ставили рейтинг 5
-select distinct imdbid from links ln inner join ratings rt on ln.movieid=rt.movieid;
+select distinct imdbid from links ln inner join ratings rt on ln.movieid=rt.movieid limit 10;
 
 --Посчитать число фильмов без оценок
 select count(1) from links ln left join ratings rt on ln.movieid=rt.movieid where rating is null group by rt.movieid;
@@ -19,7 +19,7 @@ select count(1) from links ln left join ratings rt on ln.movieid=rt.movieid wher
 select userid, avg(rating) avg_rating from ratings group by userid having avg(rating)>3.5 order by userid limit 10;
 
 --достать любые 10 imbdId из links у которых средний рейтинг больше 3.5.
-select imdbid from links where movieid in (select movieid from ratings group by movieid having avg(rating)>3.5 limit 10);
+select imdbid from links where movieid in (select movieid from ratings group by movieid having avg(rating)>3.5)  limit 10;
 
 --посчитать средний рейтинг по пользователям, у которых более 10 оценок. Нужно подсчитать средний рейтинг по все пользователям, которые попали под условие - то есть в ответе должно быть одно число.
 with active_users as (
