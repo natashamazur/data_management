2ed5f08aa8934fbca1a5d90ae11faeb7adbdf44d
natashamazur <72361552+natashamazur@users.noreply.github.com>
2020-10-26 01:00:55 +0300

    rename r to normed ratings in hw3

--- hw3.sql
+++ hw3.sql
@@ -3,7 +3,7 @@ select 'Мазур Наталья Игоревна Т120-101М-20' as FIO;
 select 
   userid, 
   movieid, 
-  (rating - min(rating) over (partition by userid)) / (max(rating) over (partition by userid) - min(rating) over (partition by userid)) as r,
+  (rating - min(rating) over (partition by userid)) / (max(rating) over (partition by userid) - min(rating) over (partition by userid)) as normed_rating,
   avg(rating) over (partition by userId) 
 from ratings 
 limit 30;
